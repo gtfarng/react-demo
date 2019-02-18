746a465a030eec34aae02b649619edeb98dae1b2
gtfarng <5735512002@psu.ac.th>
2019-02-18 17:00:42 +0700

    updater

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { randomUser } from '../redux/user'
-import logo from '../assets/images/logo.png'
-// import './App.css' // CSS version
 import './App.scss'
 
 const App = ({ user, randomUser }) => (
+  <div>
   <div className="app">
-    <img className="logo" src={logo} />
-    <img className="profile-image" src={user.image} />
-    <h1 className="name">{user.name}</h1>
-    <h2 className="email">{user.email}</h2>
-    <button className="random-button" onClick={randomUser}>random</button>
+    
+    <h1>Hello, World!</h1>
   </div>
+
+<div>
+    
+<h1>Hello, World!</h1>
+</div>
+</div>
 )
 
 const mapStateToProps = ({ user }) => ({ user })
